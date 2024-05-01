e4aae1007f8eeea9cf3c398949831a015dbaf870
Shvyrkaloff <81988933+Shvyrkaloff@users.noreply.github.com>
2024-05-02 01:39:27 +0300

    fix

--- StatusK.TestTask.FluentCalculator/Program.cs
+++ StatusK.TestTask.FluentCalculator/Program.cs
@@ -1,161 +1,189 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.ComponentModel;
 using StatusK.TestTask.FluentCalculator.Models.Operator;
 
-namespace StatusK.TestTask.FluentCalculator;
-
-public class FluentCalculator
+namespace StatusK.TestTask.FluentCalculator
 {
-
-    private StringBuilder _inputString = new();
-
-    private Stack<string> _operatorStack = new();
-
-    private StringBuilder _tempString = new();
-
-    private int _leftOperand;
-
-    private int _rightOperand;
-
-    //private static readonly Dictionary<string, int> NumberMap = new Dictionary<string, int>
-    //{
-    //    {"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4},
-    //    {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}, {"ten", 10}
-    //};
-
-    private readonly Queue<int> _operands = new Queue<int>();
-    private readonly Queue<string> _operators = new Queue<string>();
-
-    public FluentCalculator Zero => Value("0");
-    public FluentCalculator One => Value("1");
-    public FluentCalculator Two => Value("2");
-    public FluentCalculator Three => Value("3");
-    public FluentCalculator Four => Value("4");
-    public FluentCalculator Five => Value("5");
-    public FluentCalculator Six => Value("6");
-    public FluentCalculator Seven => Value("7");
-    public FluentCalculator Eight => Value("8");
-    public FluentCalculator Nine => Value("9");
-    public FluentCalculator Ten => Value("10");
-
-    public FluentCalculator Plus => Operator("+");
-    public FluentCalculator Minus => Operator("-");
-    public FluentCalculator Times => Operator("*");
-    public FluentCalculator DividedBy => Operator("/");
-
-    private FluentCalculator Value(string value)
+    public class FluentCalculator
     {
-        _inputString.Append(value);
-        return this;
-    }
+        private StringBuilder _inputString = new();
+        private Stack<string> _operatorStack = new();
+        private StringBuilder _tempString = new();
+        private int _leftOperand;
+        private int _rightOperand;
 
-    private FluentCalculator Operator(string op)
-    {
-        if (_operatorStack.Count == 0)
+        private readonly Queue<int> _operands = new Queue<int>();
+        private readonly Queue<string> _operators = new Queue<string>();
+
+        public FluentCalculator Zero => Value("0");
+        public FluentCalculator One => Value("1");
+        public FluentCalculator Two => Value("2");
+        public FluentCalculator Three => Value("3");
+        public FluentCalculator Four => Value("4");
+        public FluentCalculator Five => Value("5");
+        public FluentCalculator Six => Value("6");
+        public FluentCalculator Seven => Value("7");
+        public FluentCalculator Eight => Value("8");
+        public FluentCalculator Nine => Value("9");
+        public FluentCalculator Ten => Value("10");
+
+        public FluentCalculator Plus => Operator("+");
+        public FluentCalculator Minus => Operator("-");
+        public FluentCalculator Times => Operator("*");
+        public FluentCalculator DividedBy => Operator("/");
+
+        public FluentCalculator Value(string value)
         {
-            _operatorStack.Push(op);
-        }
-        else if(_operatorStack.Count != 0)
-        {
-            if (OperatorContainer.FindOperator(_operatorStack.Peek())!.Priority <
-                OperatorContainer.FindOperator(op)!.Priority)
+            if (IsLastCharacterOperand())
             {
-                _operatorStack.Push(op);
+                throw new InvalidOperationException("Operand cannot follow another operand");
             }
+
+            _inputString.Append(value);
+            return this;
         }
-        if (_operatorStack.Count != 0)
+
+        public FluentCalculator Operator(string op)
         {
-            if (OperatorContainer.FindOperator(_operatorStack.Peek())!.Priority >=
-                OperatorContainer.FindOperator(op)!.Priority)
+            if (IsLastCharacterOperator())
             {
-                _inputString.Append(_operatorStack.Pop());
+                throw new InvalidOperationException("Operator cannot follow another operator");
             }
 
             if (_operatorStack.Count == 0)
             {
                 _operatorStack.Push(op);
+                return this;
             }
             else if (_operatorStack.Count != 0)
             {
-                if (OperatorContainer.FindOperator(_operatorStack.Peek())!.Priority <
-                    OperatorContainer.FindOperator(op)!.Priority)
+                if (OperatorContainer.FindOperator(_operatorStack.Peek())!.Priority < OperatorContainer.FindOperator(op)!.Priority)
                 {
                     _operatorStack.Push(op);
+                    return this;
+                }
+
+                while (OperatorContainer.FindOperator(_operatorStack.Peek())?.Priority >= OperatorContainer.FindOperator(op)?.Priority)
+                {
+                    _inputString.Append(_operatorStack.Pop());
+
+                    if (!_operatorStack.Any())
+                    {
+                        _operatorStack.Push(op);
+                        break;
+                    }
                 }
             }
+
+            return this;
         }
 
-        return this;
-    }
-
-    public int Result()
-    {
-
-        while (_operatorStack.Count != 0)
+        public int Result()
         {
-            _inputString.Append(_operatorStack.Pop());
+            while (_operatorStack.Count != 0)
+                _inputString.Append(_operatorStack.Pop());
+
+            foreach (char c in _inputString.ToString())
+            {
+                if (Char.IsDigit(c))
+                {
+                    _operatorStack.Push(Convert.ToString(c));
+                    continue;
+                }
+                else if (!Char.IsDigit(c))
+                {
+
+                    if (c == '+')
+                    {
+                        _tempString.Clear();
+                        _tempString.Append(Convert.ToString(Convert.ToInt32(_operatorStack.Pop()) +
+                                                            Convert.ToInt32(_operatorStack.Pop())));
+                    }
+                    else if (c == '-')
+                    {
+                        _rightOperand = Convert.ToInt32(_operatorStack.Pop());
+                        _leftOperand = _operatorStack.Count > 0 ? Convert.ToInt32(_operatorStack.Pop()) : 0;
+
+                        _tempString.Clear();
+                        _tempString.Append(Convert.ToString(_leftOperand - _rightOperand));
+                    }
+                    else if (c == '*')
+                    {
+                        _tempString.Clear();
+                        _tempString.Append(Convert.ToString(Convert.ToInt32(_operatorStack.Pop()) *
+                                                            Convert.ToInt32(_operatorStack.Pop())));
+                    }
+                    else if (c == '/')
+                    {
+                        _rightOperand = Convert.ToInt32(_operatorStack.Pop());
+                        _leftOperand = Convert.ToInt32(_operatorStack.Pop());
+
+                        _tempString.Clear();
+                        _tempString.Append(Convert.ToString(_leftOperand / _rightOperand));
+                    }
+
+                    _operatorStack.Push(_tempString.ToString());
+                }
+            }
+            return Convert.ToInt32(_tempString.ToString());
         }
 
-        foreach (char c in _inputString.ToString())
+        public static implicit operator int(FluentCalculator calc)
         {
-            if (c == '+')
-            {
-                _tempString.Clear();
-                _tempString.Append(Convert.ToString(Convert.ToInt32(_operatorStack.Pop()) +
-                                   Convert.ToInt32(_operatorStack.Pop())));
-            }
-            else if (c == '-')
-            {
-                _rightOperand = Convert.ToInt32(_operatorStack.Pop());
-                _leftOperand = Convert.ToInt32(_operatorStack.Pop());
-
-                _tempString.Clear();
-                _tempString.Append(Convert.ToString(_leftOperand - _rightOperand));
-            }
-            else if (c == '*')
-            {
-                _tempString.Clear();
-                _tempString.Append(Convert.ToString(Convert.ToInt32(_operatorStack.Pop()) *
-                                   Convert.ToInt32(_operatorStack.Pop())));
-            }
-            else if (c == '/')
-            {
-                _rightOperand = Convert.ToInt32(_operatorStack.Pop());
-                _leftOperand = Convert.ToInt32(_operatorStack.Pop());
-
-                _tempString.Clear();
-                _tempString.Append(Convert.ToString(_leftOperand / _rightOperand));
-            }
-            _operatorStack.Push(_tempString.ToString());
+            return calc.Result();
         }
 
-        return Convert.ToInt32(_tempString);
+        private bool IsLastCharacterOperand()
+        {
+            if (_inputString.Length > 0)
+            {
+                if (_operatorStack.Count == _inputString.Length)
+                {
+                    return false;
+                }
+                else if (_operatorStack.Count + 1 == _inputString.Length)
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsLastCharacterOperator()
+        {
+            if (_inputString.Length > 0)
+            {
+                char lastChar = _inputString[^1];
+                return lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/';
+            }
+            return false;
+        }
     }
-}
 
-class Program
-{
-    public Program()
+    class Program
     {
-       
-    }
+        static void Main(string[] args)
+        {
+            OperatorContainer.AddOperator(new Operator(2, "*"));
+            OperatorContainer.AddOperator(new Operator(2, "/"));
+            OperatorContainer.AddOperator(new Operator(1, "+"));
+            OperatorContainer.AddOperator(new Operator(1, "-"));
 
-    static void Main(string[] args)
-    {
-        OperatorContainer.AddOperator(new Operator(2, "*"));
-        OperatorContainer.AddOperator(new Operator(2, "/"));
-        OperatorContainer.AddOperator(new Operator(1, "+"));
-        OperatorContainer.AddOperator(new Operator(1, "-"));
+            FluentCalculator calc = new FluentCalculator();
 
-        FluentCalculator calc = new FluentCalculator();
+            int result1 = calc.Minus.One.Plus.Two * 10;
+            Console.WriteLine(result1);
 
-        int result1 = calc.One.Plus.Two.Plus.Three.Minus.One.Minus.Two.Times.Four.Result(); // Should be -1
-        Console.WriteLine(result1);
+            //int result2 = calc.One.Plus.Ten;
+            //Console.WriteLine(result2);
 
-        int result2 = calc.One.Plus.Ten.Result(); // Should be 1
-        Console.WriteLine(result2);
-
-        int result3 = calc.One.Plus.Ten.Result(); // Should be 1
-        Console.WriteLine(result3);
+            //int result3 = calc.One.Plus.Ten;
+            //Console.WriteLine(result3);
+        }
     }
 }
